32feb435515323f619c4ee6a37fc0f287ce8b765
Akshay Rajendra Mistry <159006697+Akshay1416@users.noreply.github.com>
2024-02-09 22:09:20 +0530

    Add files via upload

---
+++ Boolean_Variable.java
@@ -0,0 +1,13 @@
+ class Boolean_Variable {
+
+    public static void main(String[] args) {
+        
+        boolean a = true; 
+        
+        System.out.println(a);
+    }
+}
+/* Output 
+CT:Success
+RT:Success:true
+*/
